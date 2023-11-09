5ba67a4f8dbe527d901d933231531677a4207aea
Naveen <naveen.satish24@gmail.com>
2023-11-09 19:45:02 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
+import 'dotenv/config'
+
+
+
+
+/*
+import express from "express";
+const app = express()
 
 ;(async () => {
     try {
         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+        app.on("error",() => {
+            console.log("ERR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+
     } catch (error) {
         console.error("ERROR: " , error);
         throw error
     }
 } )() //IIFE
+
+*/
